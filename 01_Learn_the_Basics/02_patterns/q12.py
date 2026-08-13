e59a623c7117be3c8c20948f7bdf0280bd5fec38
Rohith k <tejasrirohith@gmail.com>
2026-08-13 18:01:50 +0530

    Add pattern 12 solution

---
+++ 01_Learn_the_Basics/02_patterns/q12.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pattern12(self, n):
+        for i in range(1, n + 1):
+            
+            
+            for j in range(1, i + 1):
+                print(j, end="")
+            
+            
+            for j in range(1, 2 * (n - i) + 1):
+                print(" ", end="")
+            
+            
+            for j in range(i, 0, -1):
+                print(j, end="")
+            
+            print()
