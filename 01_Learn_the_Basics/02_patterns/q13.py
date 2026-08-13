e9f1f1daf97aa0c43ffb84edd931d008ae8730bc
Rohith k <tejasrirohith@gmail.com>
2026-08-13 18:06:02 +0530

    Add pattern 13 solution

---
+++ 01_Learn_the_Basics/02_patterns/q13.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pattern13(self, n):
+        x = 1
+        for i in range(1, n + 1):
+            for j in range(1, i + 1):
+                print(x, end=" ")
+                x += 1
+            print()
