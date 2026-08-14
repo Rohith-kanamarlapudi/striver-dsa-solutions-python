b3ab20d7e4431d659130e983cd24da93c8d5450c
Rohith k <tejasrirohith@gmail.com>
2026-08-15 00:28:40 +0530

    Add pattern 20 solution

---
+++ 01_Learn_the_Basics/02_patterns/q20.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pattern20(self, n):
+                # Upper half
+        
+        for i in range(1, n + 1):
+            print("*" * i, end="")
+            print(" " * (2 * (n - i)), end="")
+            print("*" * i)
+
+        # Lower half
+        for i in range(n-1, 0, -1):
+            print("*" * i, end="")
+            print(" " * (2 * (n - i)), end="")
+            print("*" * i)
